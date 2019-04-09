641456a61ce9954beb39ffcf4f102ca4abaace7f
Jeremy Melvin <jamelvin@gmail.com>
2019-04-09 18:20:23 -0500

    Added error catching to mpiIntegrate.

--- parallelDebugging/mpiIntegrate.cpp
+++ parallelDebugging/mpiIntegrate.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <mpi.h>
+#include <stdlib.h>
 
-double evalFxn(double pos, double leftBound, double rightBound)
+double evalFxn(double pos, double leftBound, double rightBound, int *err)
 {
-  double eval;
+  double eval=0.0;
   if ((pos >= leftBound) && (pos < 0.1))
     eval = -10.0*pos + 1;
   else if ((pos >= 0.1) && (pos <= 0.6))
     eval = 3.0*pos*pos - 2.0*pos + 0.17;
   else if ((pos > 0.6) && (pos <= rightBound))
     eval = -1.0/8.0 * (pos - 0.6) + 0.05;
-  else
+  else {
     std::cout << "Error: You are trying to integrate between " << leftBound << " and " << rightBound << " but are attempting to eval at " << pos << std::endl;
+    *err = 1;
+  }
   return eval;
 }
 
@@ -20,7 +23,7 @@ int main(int argc, char *argv[])
   int globalNumCells = 100;
   double leftBdry = 0.0;
   double rightBdry = 1.0;
-
+  int mpi_err = 0;
   double localSum = 0.0;
   double globalSum;
   int numProcs, myRank;
@@ -29,20 +32,34 @@ int main(int argc, char *argv[])
   MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
+  //bool infLoop = true;
+
+  //if (myRank == 7)
+  //  while (infLoop);
+
+  // Domain size covered by a processor
   double dProc = (rightBdry - leftBdry)/numProcs;
+  
+  // Domain size covered by a cell
   double dx = (rightBdry - leftBdry)/globalNumCells;
+
+  // On each processor, take 1/numProcs of the domain
+  // However, since you can't do a partial cell, round this
   int localNumCells = (int) ((float)globalNumCells/numProcs + 0.5);
 
   //std::cout << myRank << " " << localNumCells << " " << leftBdry + myRank*dProc << std::endl;
 
   for (int i = 0; i < localNumCells; i++)
   {
-    //double midpt = leftBdry + myRank*dProc + i*dx + 0.5*dx;
+    // Find the midpoint of the cell
     double midpt = leftBdry + myRank*localNumCells*dx + i*dx + 0.5*dx;
-    double fxnEval = evalFxn(midpt, leftBdry, rightBdry);
+    double fxnEval = evalFxn(midpt, leftBdry, rightBdry, &mpi_err);
+    if (mpi_err == 1) MPI_Abort(MPI_COMM_WORLD, 0);
+    // Midpoint rule, sum up midpoint * cell size
     localSum += fxnEval*dx;
   }
 
+  // Sum across processors
   MPI_Reduce(&localSum,&globalSum,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
 
   if (myRank == 0)
